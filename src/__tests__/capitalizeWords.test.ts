f738bd3df10551732a400d1c8487ff8827110692
Derrick Brandon <derrickdbrandon@gmail.com>
2021-10-21 18:20:03 -0400

    chore(lib): add capitalizeWords test

--- src/__tests__/capitalizeWords.test.ts
+++ src/__tests__/capitalizeWords.test.ts
@@ -1,7 +1,7 @@
 import { capitalizeWords } from '../capitalize'
 
 describe('capitalizeWords', () => {
-  it.skip('should capitalize all words in string', () => {
+  it('should capitalize all words in string', () => {
     const result = capitalizeWords('i want to be capitalized')
     expect(result).toBe('I Want To Be Capitalized')
   })
